8be123818b98ce76afaae25f31849b5643a49948
YuliyaMilk <YuliyaIse@gmail.com>
2020-10-06 20:43:19 +0300

    First commit

---
+++ src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MainPage.module.css'
+
+const MainPage = (props) => {
+    return(
+        <div className={classes.MainContent}>
+            BLABLA
+        </div>
+    )
+}
+
+export default MainPage;
