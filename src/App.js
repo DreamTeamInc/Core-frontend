2f3382dab266de28414317650cbc55b2309786d3
Klemeshov <dima-0510@mail.ru>
2020-10-08 20:03:39 +0300

    add canvas in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CanvasContainer from "./Components/Canvas/CanvasContainer";
 
 function App() {
     return (
         <div>
-
+            <CanvasContainer/>
         </div>
     );
 }
