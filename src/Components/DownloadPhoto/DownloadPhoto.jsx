311f9b3ccb9148295c6d395c6576262c6f66a016
YuliyaMilk <YuliyaIse@gmail.com>
2020-10-31 18:22:09 +0300

    you can add photos

--- src/Components/DownloadPhoto/DownloadPhoto.jsx
+++ src/Components/DownloadPhoto/DownloadPhoto.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { show_menu } from "../../Data";
 import classes from "./DownloadPhoto.module.css";
 import UploadPhotos from "./UploadPhotos/UploadPhotos";
 
 class DownloadPhoto extends React.Component {
   constructor(props) {
     super(props);
-    this.startValue = 'Выберите'
+    this.startValue = "Выберите";
     this.state = {
       firstValue: this.startValue,
       secondValue: "Скважина",
-      showComponent: false
+      showComponent: false,
+      fileList: null
     };
 
     this.firstSelectHandler = this.firstSelectHandler.bind(this);
     this.secondSelectHandler = this.secondSelectHandler.bind(this);
-    
   }
 
   firstSelectHandler(event) {
@@ -23,14 +24,14 @@ class DownloadPhoto extends React.Component {
   secondSelectHandler(event) {
     this.setState({ secondValue: event.target.value });
   }
-  
 
-  _onButtonClick (event){
+  _onButtonClick = (event) => {
     event.preventDefault();
     this.setState({
-        ...this.state,
-        showComponent:  !this.state.showComponent
-    })
+      ...this.state,
+      showComponent: !this.state.showComponent,
+      fileList: [...event.target.files]
+    });
   }
 
   render() {
@@ -44,32 +45,57 @@ class DownloadPhoto extends React.Component {
               <option hidden value={this.state.firstValue}>
                 {this.state.firstValue}
               </option>
-              <option value="2">Месторождение</option>
-              <option value="3">3</option>
-              <option value="4">Другое</option>
+              {show_menu.map((item, index) => {
+                return (
+                  <option value={item.name} key={index}>
+                    {item.name}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div className={classes.Container2}>
             <div className={classes.Text}>Скважина:</div>
-            <select onChange={this.secondSelectHandler} disabled={this.state.firstValue === this.startValue ? true: false}>
+            <select
+              onChange={this.secondSelectHandler}
+              disabled={
+                this.state.firstValue === this.startValue ? true : false
+              }
+            >
               <option hidden value={this.state.secondValue}>
                 {this.state.secondValue}
               </option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
+              {show_menu.map((item) => {
+                if (item.name === this.state.firstValue) {
+                  console.log(item.name);
+                  return item.nodes.map((el, index) => {
+                    return (
+                      <option value={el.name} key={index}>
+                        {el.name}
+                      </option>
+                    );
+                  });
+                }
+              })}
             </select>
           </div>
-        <div>
-          <button
-            className={classes.BtnDownload}
-            onClick={(event) => this._onButtonClick(event)}
-            type="button"
-          >
-            {" "}
-            Загрузить{" "}
-          </button>
-          {this.state.showComponent ?<UploadPhotos showComponent={this.state.showComponent}/> : null}
+          <div>
+            <input
+              className={classes.BtnDownload}
+              onChange={this._onButtonClick}
+              type="file"
+              id="input_file"
+              multiple
+            >
+            </input>
+            <label htmlFor="input_file">
+            <div className={classes.Input_photo}>
+              <span className={classes.Load_photo__text}>Открыть</span>
+            </div>
+          </label>
+            {this.state.showComponent ? (
+              <UploadPhotos showComponent={this.state.showComponent} fileList = {this.state.fileList}/>
+            ) : null}
           </div>
         </form>
       </div>
--- src/Components/DownloadPhoto/UploadPhotos/UploadPhotos.jsx
+++ src/Components/DownloadPhoto/UploadPhotos/UploadPhotos.jsx
@@ -1,145 +1,146 @@
-import React from 'react';
+import React from "react";
 import classes from "./UploadPhotos.module.css";
-
 class UploadPhotos extends React.Component {
-    // readFile = file => {
-    //     let fileReader = new FileReader()
-    
-    //     return new Promise((resolve, reject) => {
-    //       fileReader.onload = e => {
-    //           let dataURI = e.target.result
-    //           resolve(<ItemPreview src={dataURI} preview={true}/>)
-    //       }
-    
-    //       fileReader.onerror = () => reject('Ошибка чтения файла')
-    
-    //       if (/^image/.test(file.type)) {
-    //           fileReader.readAsDataURL(file)
-    //       } else {
-    //           reject('Можно загружать только изображения!')
-    //       }
-    //     })
-    // }
-    
-    // onAddFiles = e => {
-    //     const {files} = e.dataTransfer
-    //     this.setState({files: [...files]})
-    
-    //     let a = [].map.call(files, file => this.readFile(file))
-    
-    //     Promise.all(a)
-    //     .then(previews => this.setState({items: [...previews, ...this.state.items]}))
-    //     .catch(alert)
-    // }
-    state = {
-        inputDepth: '',
-        inputLight: ''
-      };
-    
-    handle_change = e => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState(prevstate => {
-          const newState = { ...prevstate };
-          newState[name] = value;
-          return newState;
-        });
-      };
+  // readFile = file => {
+  //     let fileReader = new FileReader()
 
-      onClose = () => {
-        this.setState({
-          showComponent: false,
-        });
-      };
+  //     return new Promise((resolve, reject) => {
+  //       fileReader.onload = e => {
+  //           let dataURI = e.target.result
+  //           resolve(<ItemPreview src={dataURI} preview={true}/>)
+  //       }
 
+  //       fileReader.onerror = () => reject('Ошибка чтения файла')
+
+  //       if (/^image/.test(file.type)) {
+  //           fileReader.readAsDataURL(file)
+  //       } else {
+  //           reject('Можно загружать только изображения!')
+  //       }
+  //     })
+  // }
+
+  // onAddFiles = e => {
+  //     const {files} = e.dataTransfer
+  //     this.setState({files: [...files]})
+
+  //     let a = [].map.call(files, file => this.readFile(file))
+
+  //     Promise.all(a)
+  //     .then(previews => this.setState({items: [...previews, ...this.state.items]}))
+  //     .catch(alert)
+  // }
+
+  constructor(props) {
+		super(props);
+		this.state = {
+			fileList: this.props.fileList
+		};
+	};
+  state = {
+    inputDepth: "",
+    inputLight: "",
+  };
+
+  handle_change = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState((prevstate) => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      showComponent: false,
+    });
+  };
+
+  handleDelete = (index) => {
+  //  this.props.fileList.splice(index,1);
+  //  console.log(this.props.fileList);
+  var arr = this.state.fileList;
+		arr.splice (index, 1);
+		this.setState ({fileList: arr});
+};
+
+  renderFiles() {
+    return (
+      <div className={classes.FormUploadPhotos}>
+      <table>
+        {" "}
+        {this.props.fileList.map((file, index) => {
+          return (
+            
+            <tr className={classes.Item} key = {index}>
+              
+              <td>
+                {" "}
+                <div className={classes.NamePhoto}>
+                  {" "}
+                  {index + 1}.{file.name}{" "}
+                </div>
+              </td>
+              <td>
+                {" "}
+                <label htmlFor="inputDepth" className={classes.inputDepth}>
+                  {" "}
+                  Глубина{" "}
+                </label>
+                <input
+                  type="text"
+                  onChange={this.handleChange}
+                  id="inputDepth"
+                  className={classes.inputDepth}
+                />
+              </td>
+              <td>
+                <label htmlFor={index+1} className={classes.inputLight}>
+                  {" "}
+                  УФ{" "}
+                </label>
+                <input
+                  type="radio"
+                  onChange={this.handleChange}
+                  name={index+1}
+                  className={classes.inputLight}
+                  value="UL"
+                />
+                <label htmlFor={index+1} className={classes.inputLight}>
+                  {" "}
+                  ДС{" "}
+                </label>
+                <input
+                  type="radio"
+                  onChange={this.handleChange}
+                  name={index+1}
+                  className={classes.inputLight}
+                  value="DL"
+                />
+              </td>
+              <td className={classes.BtnDelete} onClick={() => { this.handleDelete(index) }}> &#215; </td>
+              <br />
+            </tr>
+          );
+        })}
+      </table>
+   
+      </div>
+    );
+  }
   render() {
     return (
       <div className={classes.UploadPhotos}>
-        <div className={classes.UploadPhotosText}>Загрузка  <button className={classes.BtnExit} onClick={this.onClose}>&#215;</button></div>
-       
-        <div  className={classes.FormUploadPhotos}>
-        <table >
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 1.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 2.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 3.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 4.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 5.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 6.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-            <tr className={classes.Item}>
-              <td> <div className={classes.NamePhoto}> 7.NamePhotoCore1234567 </div></td>
-              <td> <label for="inputDepth" className={classes.inputDepth}> Глубина </label>
-               <input type="text" onChange={this.handleChange} id="inputDepth" className={classes.inputDepth}  /></td>
-               <td><label for="inputLight" className={classes.inputLight}> УФ </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="UL" />
-               <label for="inputLight" className={classes.inputLight}> ДС  </label>
-               <input type="radio" onChange={this.handleChange} name="inputLight" className={classes.inputLight} value="DL" /></td>
-               <td><button className={classes.BtnDelete}>&#215;</button></td>
-            <br/>
-            </tr>
-        </table>
-        <button className={classes.BtnSave} onClick={this.Upload} type="button"> Сохранить </button>
+        <div className={classes.UploadPhotosText}>
+          Загрузка{" "}
+          <span className={classes.BtnExit} onClick={this.onClose}>
+            &#215;
+          </span>
+        </div>
+        {this.renderFiles()}
       </div>
-     </div>
     );
   }
 }
