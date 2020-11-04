62b89f3bd701b4527b134964dde65c8e684311ec
YuliyaMilk <YuliyaIse@gmail.com>
2020-11-04 21:34:11 +0300

    update design Slider

--- src/Components/GalleryPage/GalleryLine/GalleryLine.jsx
+++ src/Components/GalleryPage/GalleryLine/GalleryLine.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
 import classes from "./GalleryLine.module.css";
+import "./slick.css";
+import "./slick-theme.css";
 import "./../../../../node_modules/slick-carousel/slick/slick.css";
 import "./../../../../node_modules/slick-carousel/slick/slick-theme.css";
 import GalleryItem from "./GalleryItem/GalleryItem";
--- src/Components/ModelsCore/ModelsGalleryLine/ModelsGalleryLine.jsx
+++ src/Components/ModelsCore/ModelsGalleryLine/ModelsGalleryLine.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
 import classes from "./ModelsGalleryLine.module.css";
+import "./slick.css";
+import "./slick-theme.css";
 import "./../../../../node_modules/slick-carousel/slick/slick.css";
 import "./../../../../node_modules/slick-carousel/slick/slick-theme.css";
 import ModelsGalleryItem from "./ModelsGalleryItem/ModelsGalleryItem";
